6d55a030826bf9e1dab3adf6ce9cb904e151f26d
hakim <akborislamamir5555.com>
2024-07-16 01:53:00 +0600

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # End-to-End-Mlops
+
+## workflows
+
+1. Update config.yaml
+2. update schema.yaml
+3. update params.yaml
+4. update the entity
+5. update the cofigaration manager in src config 
+6. update the pipeline
+8. update the main.py
+9. update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.mlproject import  logger
+from mlproject import  logger
+from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to out custome logging')
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
